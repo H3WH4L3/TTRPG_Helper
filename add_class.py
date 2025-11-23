9483c66b6d991452c0e99b81921f167e8027df00
Никита <giveyourcatsomemilk@outlook.com>
2025-11-23 03:33:37 +0300

    Initial commit

---
+++ add_class.py
@@ -0,0 +1,176 @@
+import re
+import psycopg2
+from flask import Flask, render_template, request, redirect, url_for, g
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+app = Flask(__name__)
+
+connection = psycopg2.connect(
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT"),
+)
+cursor = connection.cursor()
+
+VALIDATION_TEXT = {
+    "slug": "Только латиница, нижний регистр, слова через _",
+    "name": "Только кирриллица, слова разделяются пробелом, никаких символов",
+    "desc": "Только кориллица, допускается любой свободный форма, в конце точка",
+    "formula": "Только формат вида XdY/XdY±Z (3d6/3d6±2)",
+    "dublicate": "Такой id уже есть в базе!",
+}
+
+
+def validate_form(slug=None, name=None, describe=None, formula=None):
+    if slug:
+        return re.fullmatch(r"^[a-z]+(?:_[a-z]+)*$", slug)
+    if name:
+        return re.fullmatch(r"^[А-ЯЁ][а-яё]+(?: [А-ЯЁа-яё]+)*$", name)
+    if describe:
+        return re.fullmatch(r"^[А-ЯЁ][А-ЯЁа-яё0-9 ,:;()\-!?]*\.$", describe)
+    if formula:
+        return re.fullmatch(r"(?:(\d*)d)?(\d+)([+\-])?(\d+)?", formula)
+
+
+def execute_param(result: dict) -> tuple:
+    keys = ", ".join(result.keys())
+    placeholders = ", ".join(["%s"] * len(result))
+    return (keys, placeholders)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+# ADD BONUSES
+@app.route("/add_bonus", methods=["POST", "GET"])
+def add_bonuse():
+    cursor.execute("SELECT id, name_ru FROM classes")
+    classes = cursor.fetchall()
+
+    if request.method == "POST":
+        pass
+    else:
+        return render_template("add_bonuse.html", form={}, errors={}, classes=classes)
+
+
+# ADD NEW SKILL
+@app.route("/add_skill", methods=["POST", "GET"])
+def add_skill():
+    cursor.execute("SELECT id, name_ru FROM classes")
+    classes = cursor.fetchall()
+
+    errors = {}
+    new_skill = {}
+
+    if request.method == "POST":
+        class_id = request.form.get("class_id").strip()
+        new_skill["slug"] = request.form.get("slug", "").strip()
+        new_skill["name_ru"] = request.form.get("name_ru", "").strip()
+        new_skill["desc_ru"] = request.form.get("desc_ru", "").strip()
+
+        if not validate_form(slug=new_skill["slug"]):
+            errors["slug"] = VALIDATION_TEXT["slug"]
+
+        cursor.execute("SELECT slug FROM skills")
+        slugs_skills = [i[0] for i in cursor.fetchall()]
+        if new_skill["slug"] in slugs_skills:
+            errors["slug"] = VALIDATION_TEXT["dublicate"]
+        if not validate_form(name=new_skill["name_ru"]):
+            errors["name_ru"] = VALIDATION_TEXT["name"]
+        if not validate_form(describe=new_skill["desc_ru"]):
+            errors["desc_ru"] = VALIDATION_TEXT["desc"]
+
+        if errors:
+            return render_template(
+                "add_skill.html", errors=errors, form=new_skill, classes=classes
+            )
+
+        keys, placeholder = execute_param(new_skill)
+        cursor.execute(
+            f"INSERT INTO skills ({keys}) VALUES ({placeholder}) RETURNING id;",
+            tuple(new_skill.values()),
+        )
+        skill_id = cursor.fetchone()[0]
+        cursor.execute(
+            "INSERT INTO class_skills (class_id, skill_id) VALUES (%s, %s);",
+            (int(class_id), skill_id),
+        )
+        connection.commit()
+        return redirect(url_for("index"))
+    else:
+        return render_template("add_skill.html", form={}, errors={}, classes=classes)
+
+
+# ADD NEW CLASS
+@app.route("/add_class", methods=["POST", "GET"])
+def add_class():
+    errors = {}
+    new_class = {}
+    if request.method == "POST":
+        new_class["slug"] = request.form.get("slug", "").strip()
+        new_class["name_ru"] = request.form.get("name_ru", "").strip()
+        new_class["desc_ru"] = request.form.get("desc_ru", "").strip()
+        new_class["hp_formula"] = request.form.get("hp_formula", "").strip()
+        new_class["money_formula"] = request.form.get("money_formula", "").strip()
+        new_class["signs_formula"] = request.form.get("signs_formula", "").strip()
+        new_class["agility_formula"] = request.form.get("agility_formula", "").strip()
+        new_class["presence_formula"] = request.form.get("presence_formula", "").strip()
+        new_class["strength_formula"] = request.form.get("strength_formula", "").strip()
+        new_class["toughness_formula"] = request.form.get(
+            "toughness_formula", ""
+        ).strip()
+        new_class["bonus_type"] = request.form.get("bonus_type", "").strip()
+        new_class["weapon_formula"] = request.form.get("weapon_formula", "").strip()
+        new_class["armor_formula"] = request.form.get("armor_formula", "").strip()
+        new_class["memorie_type"] = request.form.get("memorie_type", "").strip()
+
+        if not validate_form(slug=new_class["slug"]) and "1" in new_class["slug"]:
+            errors["slug"] = VALIDATION_TEXT["slug"]
+
+        cursor.execute("SELECT slug FROM classes")
+        slugs_classes = [i[0] for i in cursor.fetchall()]
+        if new_class["slug"] in slugs_classes:
+            errors["slug"] = VALIDATION_TEXT["dublicate"]
+        if not validate_form(name=new_class["name_ru"]):
+            errors["name_ru"] = VALIDATION_TEXT["name"]
+        if not validate_form(describe=new_class["desc_ru"]):
+            errors["desc_ru"] = VALIDATION_TEXT["desc"]
+
+        for key, value in new_class.items():
+            if key.endswith("formula"):
+                if not validate_form(formula=value):
+                    errors[key] = VALIDATION_TEXT["formula"]
+            if key.endswith("type"):
+                if not validate_form(describe=value):
+                    errors[key] = VALIDATION_TEXT["desc"]
+
+        if errors:
+            return render_template("add_class.html", errors=errors, form=new_class)
+
+        keys, placeholders = execute_param(new_class)
+        cursor.execute(
+            f"""
+                    INSERT INTO classes
+                    (
+                    {keys}
+                    )
+                    VALUES ({placeholders})
+                    """,
+            tuple(new_class.values()),
+        )
+        connection.commit()
+        return redirect(url_for("index"))
+    else:
+        return render_template("add_class.html", form={}, errors={})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ main_generate.py
@@ -0,0 +1,210 @@
+import psycopg2
+import json
+from random import randint, choice
+from psycopg2.extras import DictCursor
+from dataclasses import dataclass
+import re
+import operator as op
+import os
+
+
+OPERATORS = {"+": op.add, "-": op.sub, "//": op.floordiv, "*": op.mul}
+
+connection = psycopg2.connect(
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT"),
+)
+cursor = connection.cursor(cursor_factory=DictCursor)
+
+
+@dataclass
+class Character:
+    id: str
+    name: str
+    age: int
+    sex: str
+    character_class: str
+    description: str
+    hp: int
+    money: int
+    signs: int
+    skills: dict
+    agility: int
+    presence: int
+    strength: int
+    toughness: int
+    bonus: str
+    terrible_trait: str
+    injurie: str
+    bad_habit: str
+    dangerous_past: str
+    secret_quest: str
+    memorie: str
+    armor: dict
+    weapon: dict
+    items: list
+
+
+class MBCharacter:
+    def __init__(self):
+        self.character = None
+
+    @staticmethod
+    def roll_dice(string):
+        pattern = r"(?:(\d*)d)?(\d+)([+\-])?(\d+)?"
+        match = re.fullmatch(pattern, string)
+        if match:
+            count, value, operator, bonus = match.groups()
+            count = int(count) if count else 1
+            value = int(value)
+            operator = OPERATORS.get(operator, op.add)
+            bonus = int(bonus) if bonus else 0
+            roll = sum([randint(1, value) for _ in range(count)])
+            return operator(roll, bonus)
+
+    def generate(self):
+        data = {}
+
+        # Basic
+        data["age"] = randint(18, 60)
+        data["sex"] = choice(["Мужчина", "Женщина"])
+
+        # Choose the class
+        cursor.execute("SELECT * FROM classes ORDER BY random() LIMIT 1;")
+        ch_cls = cursor.fetchone()
+        class_id = ch_cls["id"]
+
+        data["id"] = ch_cls["slug"]
+        data["character_class"] = ch_cls["name_ru"]
+        data["description"] = ch_cls["desc_ru"]
+        data["hp"] = self.roll_dice(ch_cls["hp_formula"])
+        data["money"] = self.roll_dice(ch_cls["money_formula"])
+        data["signs"] = self.roll_dice(ch_cls["signs_formula"])
+
+        # Abilities
+        data["agility"] = self.roll_dice(ch_cls["agility_formula"])
+        data["presence"] = self.roll_dice(ch_cls["presence_formula"])
+        data["strength"] = self.roll_dice(ch_cls["strength_formula"])
+        data["toughness"] = self.roll_dice(ch_cls["toughness_formula"])
+
+        # Bonus
+        data["bonus"] = {"type": ch_cls["bonus_type"]}
+
+        cursor.execute(
+            "SELECT bonus_id FROM class_bonuses WHERE class_id = %s ORDER BY random() LIMIT 1;",
+            (class_id,),
+        )
+        ch_bonus = cursor.fetchone()[0]
+
+        cursor.execute("SELECT * FROM bonuses WHERE id = %s;", (ch_bonus,))
+        ch_bonus = cursor.fetchone()
+        data["bonus"]["text"] = f"{ch_bonus["name_ru"]} : {ch_bonus["desc_ru"]}"
+
+        # Skills
+        cursor.execute(
+            "SELECT skill_id FROM class_skills WHERE class_id = %s;", (class_id,)
+        )
+        ch_skills = tuple(element[0] for element in cursor.fetchall())
+
+        cursor.execute(
+            "SELECT name_ru, desc_ru FROM skills WHERE id IN %s;", (ch_skills,)
+        )
+        ch_skills = cursor.fetchall()
+        ch_skills = {e["name_ru"]: e["desc_ru"] for e in ch_skills}
+        data["skills"] = ch_skills
+
+        # Narrative
+        cursor.execute(
+            """SELECT n.*
+            FROM (SELECT DISTINCT category FROM narrative) c
+            CROSS JOIN LATERAL (
+            SELECT *
+            FROM narrative n
+            WHERE n.category = c.category
+            ORDER BY random()
+            LIMIT 1
+            ) n
+            ORDER BY c.category;
+            """
+        )
+        ch_narrative = {e[2]: e[3] for e in cursor.fetchall()}
+        for key, value in ch_narrative.items():
+            data[key] = value
+
+        # Memories
+        cursor.execute(
+            "SELECT memorie_id FROM class_memories WHERE class_id = %s ORDER BY random() LIMIT 1;",
+            (class_id,),
+        )
+        ch_memorie = cursor.fetchone()[0]
+        cursor.execute(
+            "SELECT * FROM memories WHERE id = %s;",
+            (ch_memorie,),
+        )
+        ch_memorie = cursor.fetchone()
+        data["memorie"] = f"{ch_cls["memorie_type"]} {ch_memorie["desc_ru"]}"
+        # Armor
+        if ch_cls["armor_formula"] is None:
+            data["armor"] = {"name": "Нет брони", "level": 0, "effect": None}
+        else:
+            cursor.execute(
+                "SELECT * FROM armors WHERE id = %s;",
+                (randint(1, self.roll_dice(ch_cls["armor_formula"])),),
+            )
+            ch_armor = cursor.fetchone()
+            data["armor"] = {
+                "name": ch_armor["name_ru"],
+                "level": ch_armor["armor_level"],
+                "effect": ch_armor["effect"],
+            }
+
+        # Weapon
+        if ch_cls["weapon_formula"] is None:
+            data["weapon"] = {
+                "name": "Невооруженный",
+                "damage": "d1",
+                "effect": None,
+                "ammo": None,
+            }
+        else:
+            cursor.execute(
+                "SELECT * FROM weapons WHERE id = %s;",
+                (randint(1, self.roll_dice(ch_cls["weapon_formula"])),),
+            )
+            ch_weapon = cursor.fetchone()
+            data["weapon"] = {
+                "name": ch_weapon["name_ru"],
+                "damage": ch_weapon["damage"],
+                "effect": ch_weapon["effect"],
+                "ammo": (
+                    data["presence"] + int(ch_weapon["ammo"].split()[2])
+                    if ch_weapon["ammo"]
+                    else None
+                ),
+            }
+
+        # Items
+        data["items"] = {}
+        for element in ("first", "second", "third"):
+            cursor.execute(
+                "SELECT * FROM items WHERE category = %s ORDER BY random() LIMIT 1;",
+                (element,),
+            )
+            ch_items = cursor.fetchone()
+            data["items"][ch_items["name_ru"]] = {
+                "effect": ch_items["effect"],
+                "counts": ch_items["counts"],
+                "cost": ch_items["cost"],
+            }
+
+        self.character = Character(**data)
+
+
+test = MBCharacter()
+test.generate()
+
+for key, value in test.character.__dict__.items():
+    print(f"{key.ljust(15)} : {value}")

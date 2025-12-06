2283c5e0a8ba7bb704b4f5ffc552e6d900b889fa
H3WH4L3 <giveyourcatsomemilk@outlook.com>
2025-12-06 03:53:56 +0300

    Change 'add_class' method (Remove 'bonus' and 'memorie' blocks)

--- add_class.py
+++ add_class.py
@@ -8,7 +8,6 @@ load_dotenv()
 
 
 app = Flask(__name__)
-
 connection = psycopg2.connect(
     dbname=os.getenv("DB_NAME"),
     user=os.getenv("DB_USER"),
@@ -49,6 +48,7 @@ def index():
     return render_template("index.html")
 
 
+# ADD NEW BONUS
 @app.route("/add_bonus", methods=["POST", "GET"])
 def add_bonus():
     cursor.execute("SELECT id, name_ru FROM classes")
@@ -190,64 +190,59 @@ def add_skill():
 # ADD NEW CLASS
 @app.route("/add_class", methods=["POST", "GET"])
 def add_class():
+    if request.method == "GET":
+        return render_template("add_class.html", form={}, errors={})
+
     errors = {}
     new_class = {}
-    if request.method == "POST":
-        new_class["slug"] = request.form.get("slug", "").strip()
-        new_class["name_ru"] = request.form.get("name_ru", "").strip()
-        new_class["desc_ru"] = request.form.get("desc_ru", "").strip()
-        new_class["hp_formula"] = request.form.get("hp_formula", "").strip()
-        new_class["money_formula"] = request.form.get("money_formula", "").strip()
-        new_class["signs_formula"] = request.form.get("signs_formula", "").strip()
-        new_class["agility_formula"] = request.form.get("agility_formula", "").strip()
-        new_class["presence_formula"] = request.form.get("presence_formula", "").strip()
-        new_class["strength_formula"] = request.form.get("strength_formula", "").strip()
-        new_class["toughness_formula"] = request.form.get(
-            "toughness_formula", ""
-        ).strip()
-        new_class["bonus_type"] = request.form.get("bonus_type", "").strip()
-        new_class["weapon_formula"] = request.form.get("weapon_formula", "").strip()
-        new_class["armor_formula"] = request.form.get("armor_formula", "").strip()
-        new_class["memorie_type"] = request.form.get("memorie_type", "").strip()
 
-        if not validate_form(slug=new_class["slug"]) and "1" in new_class["slug"]:
-            errors["slug"] = VALIDATION_TEXT["slug"]
+    # Taking all values
+    new_class["slug"] = request.form.get("slug", "").strip()
+    new_class["name_ru"] = request.form.get("name_ru", "").strip()
+    new_class["desc_ru"] = request.form.get("desc_ru", "").strip()
+    new_class["hp_formula"] = request.form.get("hp_formula", "").strip()
+    new_class["money_formula"] = request.form.get("money_formula", "").strip()
+    new_class["signs_formula"] = request.form.get("signs_formula", "").strip()
+    new_class["agility_formula"] = request.form.get("agility_formula", "").strip()
+    new_class["presence_formula"] = request.form.get("presence_formula", "").strip()
+    new_class["strength_formula"] = request.form.get("strength_formula", "").strip()
+    new_class["toughness_formula"] = request.form.get("toughness_formula", "").strip()
+    new_class["weapon_formula"] = request.form.get("weapon_formula", "").strip()
+    new_class["armor_formula"] = request.form.get("armor_formula", "").strip()
 
-        cursor.execute("SELECT slug FROM classes")
-        slugs_classes = [i[0] for i in cursor.fetchall()]
-        if new_class["slug"] in slugs_classes:
-            errors["slug"] = VALIDATION_TEXT["dublicate"]
-        if not validate_form(name=new_class["name_ru"]):
-            errors["name_ru"] = VALIDATION_TEXT["name"]
-        if not validate_form(describe=new_class["desc_ru"]):
-            errors["desc_ru"] = VALIDATION_TEXT["desc"]
+    # Checking for errors
+    cursor.execute("SELECT slug FROM classes")
+    slugs_classes = [i[0] for i in cursor.fetchall()]
+    if not validate_form(slug=new_class["slug"]) and "1" in new_class["slug"]:
+        errors["slug"] = VALIDATION_TEXT["slug"]
+    if new_class["slug"] in slugs_classes:
+        errors["slug"] = VALIDATION_TEXT["dublicate"]
+    if not validate_form(name=new_class["name_ru"]):
+        errors["name_ru"] = VALIDATION_TEXT["name"]
+    if not validate_form(describe=new_class["desc_ru"]):
+        errors["desc_ru"] = VALIDATION_TEXT["desc"]
+    for key, value in new_class.items():
+        if key.endswith("formula"):
+            if not validate_form(formula=value):
+                errors[key] = VALIDATION_TEXT["formula"]
 
-        for key, value in new_class.items():
-            if key.endswith("formula"):
-                if not validate_form(formula=value):
-                    errors[key] = VALIDATION_TEXT["formula"]
-            if key.endswith("type"):
-                if not validate_form(describe=value):
-                    errors[key] = VALIDATION_TEXT["desc"]
+    # If there any errors -> Render template with error messages
+    if errors:
+        return render_template("add_class.html", errors=errors, form=new_class)
 
-        if errors:
-            return render_template("add_class.html", errors=errors, form=new_class)
-
-        keys, placeholders = execute_param(new_class)
-        cursor.execute(
-            f"""
-                    INSERT INTO classes
-                    (
-                    {keys}
-                    )
-                    VALUES ({placeholders})
-                    """,
-            tuple(new_class.values()),
-        )
-        connection.commit()
-        return redirect(url_for("index"))
-    else:
-        return render_template("add_class.html", form={}, errors={})
+    keys, placeholders = execute_param(new_class)
+    cursor.execute(
+        f"""
+                INSERT INTO classes
+                (
+                {keys}
+                )
+                VALUES ({placeholders})
+                """,
+        tuple(new_class.values()),
+    )
+    connection.commit()
+    return redirect(url_for("index"))
 
 
 if __name__ == "__main__":
